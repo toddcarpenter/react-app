dfaa0ed8ae125d8fa466ccab1ce37a72bfb455bb
Todd Carpenter <toddbc@gmail.com>
2017-04-18 11:48:14 -0400

    updates more than one state

--- public/app.jsx
+++ public/app.jsx
@@ -3,10 +3,13 @@
 // this is a child component. It is presentational
 var GreeterMessage = React.createClass({
     render: function () {
+        var name = this.props.name;
+        var message = this.props.message;
+
         return (
             <div>
-                <h1>h1 tag</h1>
-                <p>p tag</p>
+                <h1>Hello {name}!</h1>
+                <p>{message}</p>
             </div>
         );
     }
@@ -17,19 +20,31 @@ var GreeterForm = React.createClass({
     onFormSubmit: function (e) {
         e.preventDefault();
 
+        var updates = {};
+
         var name = this.refs.name.value;
+        var message = this.refs.message.value;
 
         if (name.length > 0) {
             this.refs.name.value = '';
-            this.props.onNewName(name);
+            updates.name = name;
         }
+
+        if (message.length > 0) {
+            this.refs.message.value = '';
+            updates.message = message;
+        }
+
+        this.props.onNewData(updates);
+        
     },
     render: function () {
         return (
             <div>
                 <form onSubmit={this.onFormSubmit}>
-                    <input type="text" ref="name"/>
-                    <input type="submit"/>
+                    <input type="text" ref="name"/><br/>
+                    <textarea ref="message"/><br/>
+                    <button>Submit</button>
                 </form>
             </div>
         );
@@ -42,49 +57,57 @@ var Greeter = React.createClass({
     // sets defaults for properties if noone are set on the Component
     getDefaultProps: function() {
         return {
-            name: 'React',
-            message: 'This is from a component!'
+            name: 'Default',
+            message: 'Default message from container component.'
         };
     },
     // returns an object with inital state from property provided
     // then you retrieve it with this.state.name below
     getInitialState: function () {
         return {
-            name: this.props.name
+            name: this.props.name,
+            message: this.props.message
         };
     },
     // when setting up a parent to handle the property/state, you start it with "handle"
-    handleNewName: function (name) {
-        this.setState({
-            name: name
-        });
+    handleNewData: function (updates) {
+        console.dir(updates);
+        this.setState(updates);
+
+        // if ( update.name ) {
+        //     this.setState({
+        //         name: update.name
+        //     });
+        // }
+
+        // if ( update.message ) {
+        //     this.setState({
+        //         message: update.message
+        //     });
+        // }
     },
     // the render method is the only method required for a component
     render: function() {
         var name = this.state.name;
-        var message = this.props.message;
+        var message = this.state.message;
 
         return (//open parenthesis are used so return respects the line-break. otherwise <div> would have to start on this line
-            // you can only render one root element
-            // multiple root elems will throw an error
+            // you can only render one root element. multiple root elems will throw an error
             <div>
                 {/*this is a JS expression. you can add any JS you like in here*/}
 
-                <h1>hello {name}</h1>
-                <p>p tag</p>
-
                 {/* a nested componnents */}
-                <GreeterMessage/>
+                <GreeterMessage name={name} message={message} />
 
                 {/* when setting up a child to pass the property/state, you start it with "on" */}
-                <GreeterForm onNewName={this.handleNewName}/>
+                <GreeterForm onNewData={this.handleNewData}/>
             </div>
         );
     }
 });
 
 var firstName = 'Todd',
-newMessage = 'This is from a prop!';
+newMessage = 'New message from container component.';
 
 ReactDOM.render(
     <Greeter name={firstName} message={newMessage}/>,
